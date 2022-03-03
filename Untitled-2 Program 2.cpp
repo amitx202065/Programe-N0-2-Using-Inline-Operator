d91f32bca3fd6c1b1e5cd98dedfa7daab00568e0
amitx202065 <100788013+amitx202065@users.noreply.github.com>
2022-03-03 11:21:03 +0530

    Add files via upload

---
+++ Untitled-2 Program 2.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+inline float square(float x){
+    x=x*x;
+    return (x);
+}
+int main(){
+    float num;
+    cout<<" enetr The number <float>  : ";
+    cin>>num;
+    cout<<" Its Square = "<<square(num);
+
+}
